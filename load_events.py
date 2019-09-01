f63dcd228034d823aa62bdc19bb8adfb09e476fe
Eric Porter <eric.porter1234@gmail.com>
2019-09-01 16:40:22 -0400

    add analysis

--- load_events.py
+++ load_events.py
@@ -5,6 +5,8 @@ import os
 import re
 import pickle
 import os.path
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_history(start_dt, end_dt, last_scores):
     pleco_db = sqlite3.connect("data/{}/Pleco Flashcard Database.pqb".format(end_dt.strftime("%Y-%m-%d %H.%M.%S")))
@@ -34,7 +36,6 @@ def get_history(start_dt, end_dt, last_scores):
                 continue
             num_rvw = int(num_rvw - last_num_rvw)
             hist = row['history'][-num_rvw:]
-            # print(hist)
         else:
             rvw_start_ts = row['firstreviewedtime']
             rvw_end_ts = row['lastreviewedtime']
@@ -48,7 +49,7 @@ def get_history(start_dt, end_dt, last_scores):
         interval = dur / num_rvw
         for i in range(num_rvw):
             review_ts = rvw_start_ts + (interval * i)
-            result = int(hist[i]) <= 3
+            result = int(hist[i]) >= 4
             ts_is_exact = (rvw_start_ts == row['firstreviewedtime'] and i == 0) or i == num_rvw - 1
             dt_is_exact = ts_is_exact or rvw_start_ts.date() == rvw_end_ts.date()
             history = history.append({'review_ts': review_ts, 'headword': row['hw'], 'dictid': row['dictid'], 'dictentry': row['dictentry'], 'result': result, 'ts_is_exact': ts_is_exact, 'dt_is_exact': dt_is_exact}, ignore_index=True)
@@ -101,3 +102,38 @@ def save_history(full_history, last_dt, last_scores):
 full_history, last_dt, last_scores = load_history()
 full_history, last_dt, last_scores = get_full_history(full_history, last_dt, last_scores)
 save_history(full_history, last_dt, last_scores)
+
+full_history.info()
+
+prep = full_history.copy()
+prep['review_dt'] = pd.to_datetime(prep['review_ts']).apply(lambda x: x.date())
+prep = prep[['headword', 'review_dt', 'result', 'review_ts']]
+prep = prep.sort_values(['headword', 'review_dt', 'review_ts'])
+prep.reset_index(drop=True, inplace=True)
+
+prep['n_correct'] = prep.groupby(['headword'])['result'].apply(lambda x: x.cumsum())
+prep['n_incorrect'] = prep.groupby(['headword'])['result'].apply(lambda x: x.apply(lambda y: not y).cumsum())
+prep['last_result'] = prep.groupby(['headword', 'review_dt'])['result'].transform('last')
+prep['learned'] = prep[['n_correct', 'n_incorrect', 'last_result']].apply(lambda x: x[0] > x[1] and x[2], axis=1)
+prep['lag_learned'] = prep.groupby(['headword'], as_index=False)['learned'].shift(1)
+
+def get_net_learned(learned, lag_learned):
+    if (not pd.isna(lag_learned) and learned == lag_learned) or (pd.isna(lag_learned) and not learned):
+        return 0
+    if learned:
+        return 1
+    return -1
+
+prep['net_learned'] = prep[['learned', 'lag_learned']].apply(lambda x: get_net_learned(x[0], x[1]), axis=1)
+
+n_reviewed = prep.groupby('review_dt')['headword'].count().rename('n_reviewed')
+new_vocab = prep.groupby(['headword'], as_index=False)['review_dt'].min().groupby(['review_dt'])['headword'].count().rename('new_vocab')
+net_learned = prep.groupby('review_dt')['net_learned'].sum()
+
+review = pd.DataFrame({'n_reviewed': n_reviewed, 'new_vocab': new_vocab, 'net_learned': net_learned})
+review['new_vocab'] = review['new_vocab'].apply(lambda x: 0 if pd.isna(x) else x)
+review['vocab_size'] = review['new_vocab'].cumsum()
+review['total_learned'] = review['net_learned'].cumsum()
+
+chart = review.plot.line(subplots=True)
+
